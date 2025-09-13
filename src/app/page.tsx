44634fe6e588cb3c6b0d36dd8f8f27618fe09324
Abzal Orazbek <abzalorazbek2003@gmail.com>
2025-09-13 16:09:40 +0500

    add google maps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import GoogleMap from "../components/GoogleMap";
 
 export default function Home() {
   return (
     <div>
-      
+      <div style={{ position: "fixed", inset: 0, width: "100vw", height: "100vh", zIndex: 0, margin: "5px" }}>
+        <GoogleMap
+          center={{ lat: 51.08916576942158, lng: 71.416075309543 }}
+          zoom={15}
+        />
+      </div>
     </div>
   );
 }
